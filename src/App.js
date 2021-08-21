53d3c195b0ec0e1636b814fba2fad62b55ab4701
zsoe <zwemsoe@gmail.com>
2021-08-21 16:20:38 -0700

    without localstorage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleAdd = (e) => {
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
+  const handleDelete = (index) => {
+    const new_todos = [...todos]; // A new copy of current todos must be created
+    new_todos.splice(index, 1);
+    setTodos(new_todos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <input
+          className='input-field'
+          type='text'
+          placeholder='Enter your todo item'
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        ></input>
+        <button onClick={handleAdd}>Add</button>
+
+        <ul>
+          {todos.map((item, index) => {
+            return (
+              <div key={item} className='row'>
+                <li>{item}</li>
+                <button onClick={() => handleDelete(index)}>Delete</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
